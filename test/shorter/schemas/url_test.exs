0e641e883047ce7304bb45761ceee500dafb4b6a
jennie232 <jennie.youn87r@gmail.com>
2024-03-30 13:10:42 -0400

    Expand test cases for validate_url and validate_required

--- test/shorter/schemas/url_test.exs
+++ test/shorter/schemas/url_test.exs
@@ -5,7 +5,8 @@ defmodule Shorter.Schemas.UrlTest do
   alias Shorter.Schemas.Url
 
   describe "URL validations" do
-    @valid_attrs %{original_url: "https://validurl.com"}
+    @valid_attrs %{original_url: "https://validurl.com", slug: "slug123"}
+    @invalid_attrs %{original_url: "ht://invalidurl", slug: nil}
 
     test "with valid attributes" do
       changeset = Url.changeset(%Url{}, @valid_attrs)
@@ -13,21 +14,50 @@ defmodule Shorter.Schemas.UrlTest do
     end
 
     test "is invalid if scheme is other than http/https" do
-      changeset = Url.changeset(%Url{}, %{original_url: "pps://invalidurl.com"})
+      changeset = Url.changeset(%Url{}, %{@invalid_attrs | slug: "slug123"})
       refute changeset.valid?
       assert errors_on(changeset) == %{original_url: ["URL scheme must be 'http' or 'https'"]}
     end
 
     test "is invalid if URL does not have host" do
-      changeset = Url.changeset(%Url{}, %{original_url: "https://"})
+      changeset = Url.changeset(%Url{}, %{@valid_attrs | original_url: "https://"})
       refute changeset.valid?
-      assert errors_on(changeset) == %{original_url: ["URL does not have host"]}
+      assert errors_on(changeset) == %{original_url: ["URL does not have a valid host"]}
+    end
+
+    test "is invalid if URL does not have a valid domain extension" do
+      changeset = Url.changeset(%Url{}, %{@valid_attrs | original_url: "https://invalid"})
+      refute changeset.valid?
+      assert errors_on(changeset) == %{original_url: ["URL does not have a valid host"]}
     end
 
     test "is invalid if no original URL as input" do
       changeset = Url.changeset(%Url{}, %{})
       refute changeset.valid?
-      assert errors_on(changeset) == %{original_url: ["can't be blank"]}
+      assert errors_on(changeset) == %{original_url: ["can't be blank"], slug: ["can't be blank"]}
+    end
+
+    test "is invalid if slug is nil" do
+      changeset = Url.changeset(%Url{},  %{@valid_attrs | slug: nil})
+      refute changeset.valid?
+      assert errors_on(changeset) == %{slug: ["can't be blank"]}
+    end
+
+    test "is invalid if slug is empty" do
+      changeset = Url.changeset(%Url{},  %{@valid_attrs | slug: ""})
+      refute changeset.valid?
+      assert errors_on(changeset) == %{slug: ["can't be blank"]}
+    end
+
+    test "is invalid with duplicate slug" do
+      # Insert a URL with the same slug
+      %Url{}
+      |> Url.changeset(%{original_url: "https://valid.com", slug: "slug123"})
+      |> Repo.insert!()
+
+      changeset = Url.changeset(%Url{}, @valid_attrs)
+      assert {:error, changeset} = Repo.insert(changeset)
+      assert errors_on(changeset) == %{slug: ["Slug already exists"]}
     end
   end
 end
