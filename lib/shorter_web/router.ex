d08436a932a2471df317b5e82706b219ec278b77
jennie232 <jennie.youn87r@gmail.com>
2024-04-03 14:37:26 -0400

    Fix router.ex regarding CSRF

--- lib/shorter_web/router.ex
+++ lib/shorter_web/router.ex
@@ -6,7 +6,7 @@ defmodule ShorterWeb.Router do
     plug :fetch_session
     plug :fetch_live_flash
     plug :put_root_layout, html: {ShorterWeb.Layouts, :root}
-    # plug :protect_from_forgery
+    plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
 
